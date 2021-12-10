a9082f0ae2bb5aeea6dc109d8371260718a25a00
Juan Carlos Trimino <juancarlos@trimino.com>
2021-12-10 12:28:06 -0600

    Change module name; use random_id to generate postfix strings for buckets.

--- tf/ibm-cos/create-bucket/main.tf
+++ tf/ibm-cos/create-bucket/main.tf
@@ -1,7 +1,18 @@
-module "bucket" {
+module "bucket1" {
   # Specify the location of the module, which contains the file main.tf.
   source = "../modules/bucket"
-  bucket_name = "${var.bucket_name}-${random_string.unique-string.result}"
+  bucket_name = "${var.bucket_name}-${random_id.postfix-bucket[0].hex}"
+  resource_group_id = data.ibm_resource_group.rg.id
+  plan = var.plan
+  location = var.location
+  region_location = var.region_location
+  storage_class = var.storage_class
+}
+
+module "bucket2" {
+  # Specify the location of the module, which contains the file main.tf.
+  source = "../modules/bucket"
+  bucket_name = "${var.bucket_name}-${random_id.postfix-bucket[1].hex}"
   resource_group_id = data.ibm_resource_group.rg.id
   plan = var.plan
   location = var.location
