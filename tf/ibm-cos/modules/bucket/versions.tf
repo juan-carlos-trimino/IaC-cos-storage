ddbf7ed9eddbd8e384a9909548e9d60cf99f67a4
Juan Carlos Trimino <juancarlos@trimino.com>
2021-12-10 23:08:39 -0600

    Clean code.

--- tf/ibm-cos/modules/bucket/versions.tf
+++ tf/ibm-cos/modules/bucket/versions.tf
@@ -1,9 +1,9 @@
-##################################################
-# https://registry.terraform.io/browse/providers #
-####################################################################################################
-# The ~> operator is a convenient shorthand for allowing only patch releases within a specific     #
-# minor release.                                                                                   #
-####################################################################################################
+###################################################################################################
+# https://registry.terraform.io/browse/providers                                                  #
+###################################################################################################
+# The ~> operator is a convenient shorthand for allowing only patch releases within a specific    #
+# minor release.                                                                                  #
+###################################################################################################
 terraform {
   # Terraform version.
   required_version = ">= 1.0.0"
